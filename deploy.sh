5a634484c6be0691f4887146452b57a6ec65df56
freijea <jsfreitas_jean@hotmail.com>
2022-08-20 16:28:15 -0300

    config 4

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t jsfreitas/multi-client:latest - t jsfreitas/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t jsfreitas/multi-worker:latest - t jsfreitas/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker build -t jsfreitas/multi-server:latest - t jsfreitas/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jsfreitas/multi-client:latest -t jsfreitas/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jsfreitas/multi-worker:latest -t jsfreitas/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t jsfreitas/multi-server:latest -t jsfreitas/multi-server:$SHA -f ./server/Dockerfile ./server
 
 docker push jsfreitas/multi-client:latest
 docker push jsfreitas/multi-worker:latest
